a389896e6d8e80b8775a89ccc4bb80bfa7a6d0a3
liyisheng <liyisheng@kaikeba.com>
2022-08-23 16:27:59 +0800

    ceshi

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,9 +15,18 @@ module.exports = {
           {text: 'css', link: '/基础知识/css.md'},
           {text: '网络', link: '/基础知识/网络.md'},
           {text: '浏览器', link: '/基础知识/浏览器.md'},
+          {text: 'Git', link: '/基础知识/Git.md'},
           {text: '后端', link: '/基础知识/后端.md'},
           {text: '计算机', link: '/基础知识/计算机.md'},
           {text: '图形学', link: '/基础知识/图形学.md'}
+        ]
+      },
+      {
+        text: '框架',
+        ariaLabel: '框架',
+        items: [
+          {text: 'Webpack', link: '/框架/webpack.md'},
+          {text: 'React', link: '/框架/react.md'}
         ]
       }
     ],
@@ -29,10 +38,15 @@ module.exports = {
         'css',
         '网络',
         '浏览器',
+        'Git',
         '后端',
         '计算机',
         '图形学'
         
+      ],
+      '/框架/': [
+        'webpack',
+        'react'
       ]
     } 
   },
